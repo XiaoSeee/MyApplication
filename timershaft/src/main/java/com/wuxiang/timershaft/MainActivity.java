daabe506b5701b6e1a29aa572b67be7134518577
XiaoSe <tcboy1989@126.com>
2015-08-21 16:33:33 +0800

    in to

---
+++ timershaft/src/main/java/com/wuxiang/timershaft/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wuxiang.timershaft;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Xiang on 2015/8/21.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
