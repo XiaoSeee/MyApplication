477cb41df78215ddb687f2b3da0fa83aeb9b406d
XiaoSe <tcboy1989@126.com>
2015-08-22 23:01:54 +0800

    0822ï¼æä¸æäº¤ä»£ç 

--- timershaft/src/main/java/com/wuxiang/timershaft/MainActivity.java
+++ timershaft/src/main/java/com/wuxiang/timershaft/MainActivity.java
@@ -2,16 +2,28 @@ package com.wuxiang.timershaft;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+
+import com.meizu.flyme.reflect.StatusBarProxy;
 
 /**
  * Created by Xiang on 2015/8/21.
  * 首界面
  */
 public class MainActivity extends Activity {
+    public TabLayout mTopTable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        //魅族设置状态栏图标文字为深色
+        StatusBarProxy.setStatusBarDarkIcon(getWindow(), true);
+        mTopTable = (TabLayout) findViewById(R.id.main_tab);
+        mTopTable.addTab(mTopTable.newTab().setIcon(R.mipmap.ic_alarm_black));
+        mTopTable.addTab(mTopTable.newTab().setIcon(R.mipmap.ic_alarm_black));
+        mTopTable.addTab(mTopTable.newTab().setIcon(R.mipmap.ic_alarm_black));
+        mTopTable.addTab(mTopTable.newTab().setIcon(R.mipmap.ic_alarm_black));
         //测试没有代理能否提交
     }
 }
